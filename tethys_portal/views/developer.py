b5503f16b3683064aec4094f3d7384049ac42fdc
Nathan Swain <nathan.swain86@gmail.com>
2014-12-10 16:26:58 -0700

    Started a GitHub Pages branch

--- tethys_portal/views/developer.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import HttpResponse, render
-
-
-def home(request):
-
-    context = {}
-    return render(request, 'tethys_portal/developer/home.html', context)
--- tethys_portal/views/home.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'tethys_portal/home.html', {})
